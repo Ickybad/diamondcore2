13935b81fff08a6bc92cbc3517f9a7e3dd769450
Fabian <fabian@easy-emu.de>
2010-03-24 14:53:46 +0100

    [105] Fixed crash caused by wrong spell_dbc values.

---
+++ sql/Updates/105_01_world_spell_dbc.sql
@@ -0,0 +1,12 @@
+ALTER TABLE `spell_dbc`
+DROP COLUMN `EffectBaseDice1`;
+ALTER TABLE `spell_dbc`
+DROP COLUMN `EffectBaseDice2`;
+ALTER TABLE `spell_dbc`
+DROP COLUMN `EffectBaseDice3`;
+ALTER TABLE `spell_dbc`
+DROP COLUMN `EffectDicePerLevel1`;
+ALTER TABLE `spell_dbc`
+DROP COLUMN `EffectDicePerLevel2`;
+ALTER TABLE `spell_dbc`
+DROP COLUMN `EffectDicePerLevel3`;
